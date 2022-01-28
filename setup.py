2dd496e22480be479b92e1b970f0b373306e5ee8
vladimir <gavrilov.vlad.tim@gmail.com>
2022-01-29 00:16:50 +0300

    Добавил автора в описание установки

--- setup.py
+++ setup.py
@@ -4,10 +4,11 @@ with open('requirements.txt') as reqs_file:
     reqs = [str(line.strip()) for line in reqs_file.readlines()]
 
 setup(
-    name='websbor_client',
+    name='websbor-client',
     version='0.1',
     description='Клиент для WEB API сервиса федеральной службы государственной статистики',
     packages=['websbor'],
+    author='gavrilov.vlad.tim@gmail.com',
     url='https://github.com/gavrilov-vlad-tim/orgs_legal_info.git',
     install_requires=reqs
 )
